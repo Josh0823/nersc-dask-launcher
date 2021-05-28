fd17bd13561387d2d83d5da25edbed1103d44d98
Josh0823 <31145777+Josh0823@users.noreply.github.com>
2021-05-28 14:10:41 -0400

    Added error handling

--- dask_launcher.py
+++ dask_launcher.py
@@ -18,20 +18,31 @@ class DaskLauncher():
         image = time = ntasks = cpus = ''
 
         self.title = HTML('<h3>Dask Launcher Config</h3>')
-        self.image = widgets.Text(description='Image', placeholder='None', value=image)
+        self.image = widgets.Text(description='Shifter Img', placeholder='None', value=image)
         self.time = widgets.Text(description='Time', placeholder='30', value=time)
         self.ntasks = widgets.Text(description='NTasks', placeholder='32', value=ntasks)
         self.cpus = widgets.Text(description='CPUs/Task', placeholder='2', value=cpus)
         self.button = widgets.Button(description="Launch Dask")
         self.button.on_click(self.handle_submit)
         self.output = widgets.Output()
-        
+
         self.client = None
         self._button_clicked = False
-        
+    
+    def display(self):
         display(self.title, self.image, self.time, self.ntasks, self.cpus, self.button, self.output)
-        
+
     def handle_submit(self, b):
+        if self._button_clicked:
+            err = ('Error: you have already clicked Launch Dask. '
+                   'You can reload the widget to try to relaunch, but check my.nersc.gov '
+                   'to ensure you don\'t have unknown jobs running.')
+            with self.output:
+                raise Exception(err)
+            raise Exception(err)
+        
+        self.validate_input()
+        
         cmd = self.format_command()
 
         cmd_prefix = f'cd {os.environ["SCRATCH"]} && module load nersc-dask && '
@@ -45,21 +56,30 @@ class DaskLauncher():
         self._button_clicked = True
 
         self.print_display(out)
-    
+        
+    def validate_input(self):
+        if self.time.value != '' and (not self.time.value.isnumeric() or float(self.time.value) < 0):
+            raise Exception('ValueError: invalid time value')
+        if not self.ntasks.value.isdigit() and self.ntasks.value != '':
+            raise Exception('ValueError: invalid ntasks value')
+        if not self.cpus.value.isdigit() and self.cpus.value != '':
+            raise Exception('ValueError: invalid cpus per task value')
+
     def print_display(self, out):
         clear_output()
         with self.output:
             clear_output()
             print(f'({now()}) Launching Dask using the following salloc call:')
             print(out.decode('utf-8').replace('Test only, exiting', ''))
-            
+
             print('Run the cell below to connect your Dask client. This may take a couple minutes.')
-            print('You can view the status of your cluster job at my.nersc.gov')
+            print('You can view the status of your Dask job at my.nersc.gov')
 
     def get_client(self):
         if self._button_clicked:
-            scheduler_file = os.path.join(os.environ["SCRATCH"], "scheduler.json")
-            dask.config.config["distributed"]["dashboard"]["link"] = "{JUPYTERHUB_SERVICE_PREFIX}proxy/{host}:{port}/status"
+            scheduler_file = os.path.join(os.environ["SCRATCH"], 'scheduler.json')
+            url = '{JUPYTERHUB_SERVICE_PREFIX}proxy/{host}:{port}/status'
+            dask.config.config["distributed"]["dashboard"]["link"] = url
 
             self.client = Client(scheduler_file=scheduler_file)
             return self.client
